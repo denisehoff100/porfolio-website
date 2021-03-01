04943c676d9a7f3128637cdb5cbfcfe192503dab
denisehoff100 <66570208+denisehoff100@users.noreply.github.com>
2021-02-28 21:21:50 -0500

    Add menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Thank-you for reviewing my portfolio!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
